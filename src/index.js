55f63d1fd18280aa30b983d7b6c0d9577b673e8c
uno-124 <83322467+uno-124@users.noreply.github.com>
2021-05-13 16:49:46 +0900

    5-32途中

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // const inputText = document.getElementById("add-text").value;
+  // alert(inputText);
+  alert("aiueo");
+};
+
+// 追加ボタンをクリックされた時にonclickAdd()が呼ばれるようにする
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
